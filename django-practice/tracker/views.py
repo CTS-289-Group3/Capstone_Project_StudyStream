72caf0b0651e3986092d3c78fe4a64f3bad775ee
VerryBerryBear <sophiahays55@gmail.com>
2026-03-15 17:32:34 -0400

    Add login, logout, and register pages

--- django-practice/tracker/views.py
+++ django-practice/tracker/views.py
@@ -1,4 +1,34 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'tracker/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'tracker/register.html', {'form': form})
+
+@login_required
 def home(request):
-    return HttpResponse("Hello from your Django practice project!")
+    return render(request, 'tracker/home.html')
