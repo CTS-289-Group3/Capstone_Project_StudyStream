a2e27e7386a48e5e899047f21f4e58e386698a1c
Hearnt2538 <hearnt2538@student.faytechcc.edu>
2026-03-24 14:25:27 -0400

    feat: add recurring locations, job titles, personal events; unify personal event form; add database ERD doc

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import PersonalEvent, WorkShift
+from .models import (
+	PersonalEvent,
+	RecurringPersonalEvent,
+	RecurringJobTitle,
+	RecurringWorkLocation,
+	RecurringWorkShift,
+	WorkShift,
+)
 from .models import Workspace
 
 
 admin.site.register(PersonalEvent)
 admin.site.register(WorkShift)
+admin.site.register(RecurringWorkShift)
+admin.site.register(RecurringPersonalEvent)
+admin.site.register(RecurringWorkLocation)
+admin.site.register(RecurringJobTitle)
 admin.site.register(Workspace)
